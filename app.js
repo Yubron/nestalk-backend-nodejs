1f1a853ba7ce1f3ada38b9d3f17dffbe5475f247
yubron <wlstn4966@gmail.com>
2021-04-08 00:07:57 +0900

    📝 Feat: roomRouter 개발 ( CRUD )

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const chatRouter = require('./chatRouter');
-
-
+const roomRouter = require('./roomRouter');
+const bodyParser = require('body-parser');
 app.use(morgan('dev'));
 
 // VIEW ENGINE HTML SET-UP
@@ -13,6 +13,10 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(__dirname + '/tempClient'));
 
+app.use(bodyParser.urlencoded({ extended: false })); // urlencoded parse
+app.use(bodyParser.json()); // json을 parse하도록 함
+
 app.use('/', chatRouter);
+app.use('/room/', roomRouter);
 
 module.exports = app;
